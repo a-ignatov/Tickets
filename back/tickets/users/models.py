08ff3a66d5839bfa27c3aeb5fc9ac11cacc04ba7
Anton Ignatov <a.ignatov2@yandex.ru>
2023-02-28 18:35:44 +0300

    2nd

--- back/tickets/users/models.py
+++ back/tickets/users/models.py
@@ -1,3 +1,50 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from tickets.settings import MAX_FIRSTNAME_LASTNAME_LENGTH
+
+
+class User(AbstractUser):
+    USER = 'user'
+    ADMIN = 'admin'
+
+    USER_ROLE = (
+        (USER, 'User'),
+        (ADMIN, 'Administrator'),
+    )
+
+    email = models.EmailField('Email',
+                              unique=True,
+                              error_messages={
+                                  'unique':
+                                  ('The email is already registered'),
+                              })
+
+    first_name = models.TextField(
+        'First Name',
+        max_length=MAX_FIRSTNAME_LASTNAME_LENGTH,
+        blank=True,
+    )
+
+    last_name = models.TextField(
+        'Last Name',
+        max_length=MAX_FIRSTNAME_LASTNAME_LENGTH,
+        blank=True,
+    )
+
+    bio = models.TextField(
+        'Bio',
+        blank=True,
+    )
+
+    role = models.CharField('User role',
+                            max_length=15,
+                            choices=USER_ROLE,
+                            default=USER)
+
+    @property
+    def is_admin(self):
+        return self.role == self.ADMIN or self.is_superuser
+
+    def __str__(self):
+        return self.email
